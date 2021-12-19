68125d24c61b8c793e276a2abe70a61082727e50
Devishi Ranjan <drconker@gmail.com>
2021-12-19 00:29:05 -0500

    Updated pagination + added chronological order fix

--- theme4/theme4.js
+++ theme4/theme4.js
@@ -1,3 +1,48 @@
+// CHRONO COMIC PAGINATION CORRECTION
+// Helpers
+function swapPrevNext(previouspage, nextpage) {
+	const prevurl = previouspage.attr("href");
+	previouspage.attr("href", nextpage.attr("href"));
+	nextpage.attr("href", prevurl);
+}
+function updateLink(oldLink, newHTML, newLink, oldclass, newclass) {
+	oldLink.html(newHTML)
+		   .attr("href", newLink)
+		   .toggleClass(oldclass + " " + newclass);
+}
+// Main
+function chronoCheck(firstHTML, prevHTML, nextHTML, lastHTML, totalpages, firstURL) {
+	const url = window.location.href;
+
+	if (url.search(/\/tagged\/.*\/chrono/) > -1) {
+		const arr = url.split("/");
+		const first = $(".pagination .firstpage");
+		const prev = $(".pagination .prevpage");
+		const next = $(".pagination .nextpage");
+		const last = $(".pagination .lastpage");
+		
+		if (prev.length && next.length) {
+			swapPrevNext(prev, next);
+			
+			if (last.length) { last.attr("href", "/tagged/" + arr[arr.length - 4] + "/chrono/page/" + totalpages); }
+		}
+		
+		else if (prev.length) {
+			// Change to next/latest
+			updateLink(first, nextHTML, prev.attr("href"), "firstpage", "nextpage");
+			
+			updateLink(prev, lastHTML, "/tagged/" + arr[arr.length - 2] + "/chrono/page/" + totalpages, "prevpage", "lastpage");
+		}
+		
+		else if (next.length) {
+			// Change to first/prev
+			updateLink(last, prevHTML, next.attr("href"), "lastpage", "prevpage");
+			
+			updateLink(next, firstHTML, firstURL, "nextpage", "firstpage");
+		}
+	}
+};
+
 $(document).ready( function() {
 
 	// audio post setup
