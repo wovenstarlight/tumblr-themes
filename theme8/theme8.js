d306201b4f75609279463fdb4cc3ce9e20de9f97
Devishi Ranjan <drconker@gmail.com>
2022-04-25 08:49:05 -0400

    Init: tabs, tooltips, audio/photoset, p tags

---
+++ theme8/theme8.js
@@ -0,0 +1,31 @@
+$(document).ready( function() {
+
+	// switch between tabs
+	$("#tapes button").click( function() {
+		$("#tapes button").removeClass("active");
+		$(this).addClass("active");
+		$("#main section").fadeOut();
+		$("#" + $(this).attr("data-page")).fadeIn();
+	});
+
+	// set up tooltips
+	$("[title]").style_my_tooltips();
+
+	// audio post setup
+	customAudio({
+		playButton: "<i class='cp cp-volume'></i>",
+		pauseButton: "<i class='cp cp-mute'></i>",
+		errorIcon: "<i class='cp cp-cross'></i>",
+		callAfterLoad: null
+	});
+
+	// photoset setup
+	npfPhotosets("article", {
+		includeCommonPhotosets: true,
+		photosetMargins: "2"
+	});
+
+	// Remove stray paragraph tags
+	$(".bodyitemcontent p:first-child:empty").remove();
+
+});
